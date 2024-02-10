8ea30301335290ca6dd6c67dece5c72cfdd1add9
hsbnsultan <hasibuan.sultann@gmail.com>
2024-02-10 21:56:20 +0700

    inspect commit

---
+++ assets/js/inspect.js
@@ -0,0 +1,19 @@
+// Mencegah munculnya menu konteks saat mengklik kanan
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+});
+
+// Mengatur interval untuk memeriksa apakah jendela inspeksi terbuka
+var checkInspectorInterval = setInterval(function() {
+    if ((window.outerWidth - window.innerWidth) > 100 || (window.outerHeight - window.innerHeight) > 100) {
+        alert('Tidak dapat membuka inspeksi');
+        clearInterval(checkInspectorInterval);
+    }
+}, 1000);
+
+// Mencegah akses ke inspeksi menggunakan shortcut keyboard
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I')) {
+        e.preventDefault();
+    }
+});
